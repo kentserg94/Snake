33e12a9222cb0e95e8d6235272ca879b67ee26aa
kentserg1994 <kentserg1994@mail.ru>
2016-06-12 19:23:41 +0300

    the script creating new DOM elements added, mini bags fixed

---
+++ newsnake/draw.js
@@ -0,0 +1,18 @@
+function draw() {
+    var score=document.createElement("div");
+    score.className="score";
+    document.body.appendChild(score);
+    var point=document.createElement("h2");
+    point.setAttribute("id","point");
+    score.appendChild(point);
+    var field = document.createElement("div");
+    field.setAttribute("id","field");
+    document.body.appendChild(field);
+    var snake=document.createElement("div");
+    snake.setAttribute("id","snake");
+    field.appendChild(snake);
+    var food=document.createElement("div");
+    food.setAttribute("id","food");
+    field.appendChild(food);
+}
+draw();
